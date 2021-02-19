9fc970ed01f41e254f844ad7903b2ce3110326a1
SamrikaSingh <samrika666@gmail.com>
2021-02-20 00:07:28 +0530

    practice push code

---
+++ src/main/java/Samrikaw/App.java
@@ -0,0 +1,11 @@
+package Samrikaw;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello samrika in java world");
+
+	}
+
+}
---
+++ src/test/java/Samrikaw/AppTest.java
@@ -0,0 +1,15 @@
+package Samrikaw;
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
